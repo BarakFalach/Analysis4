f2857bd84387cbadb54f81e46475a2bf0d2fcbc9
BarakFalach <barak8995@gmail.com>
2020-12-27 11:35:43 -0800

    done notinh

--- src/Device.java
+++ src/Device.java
@@ -1,22 +1,44 @@
 import java.util.ArrayList;
 
 public class Device {
+    private float price;
+
+
+    public String name;
     private int minWeight;
     private int minHeight;
     private int minAge;
     private boolean isOpen;
     private final ArrayList<eTicket> eTickets;
 
-    public Device(int minWeight, int minHeight, int minAge, boolean isOpen) {
+    public Device(int minWeight, int minHeight, int minAge, boolean isOpen,String name,float price) {
         this.minWeight = minWeight;
         this.minHeight = minHeight;
         this.minAge = minAge;
         this.isOpen = isOpen;
         this.eTickets = new ArrayList<>();
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void addETicket(eTicket ticket){
         this.eTickets.add(ticket);
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
     }
 
 
--- src/extremeDevice.java
+++ src/extremeDevice.java
@@ -1,5 +1,5 @@
 public class extremeDevice extends Device {
-    public extremeDevice(int minWeight, int minHeight, int minAge, boolean isOpen) {
-        super(minWeight, minHeight, minAge, isOpen);
+    public extremeDevice(int minWeight, int minHeight, int minAge, boolean isOpen, String name, float price) {
+        super(minWeight, minHeight, minAge, isOpen, name, price);
     }
 }

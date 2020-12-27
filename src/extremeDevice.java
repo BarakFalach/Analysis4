b8b14cd42d17608474e357d4370f5e37a4611202
BarakFalach <barak8995@gmail.com>
2020-12-27 09:27:18 -0800

    device , braclet and Extreemdevices

---
+++ src/extremeDevice.java
@@ -0,0 +1,5 @@
+public class extremeDevice extends Device {
+    public extremeDevice(int minWeight, int minHeight, int minAge, boolean isOpen) {
+        super(minWeight, minHeight, minAge, isOpen);
+    }
+}

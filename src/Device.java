f961564e9741103f218b0e9de771f0919d97502e
BarakFalach <barak8995@gmail.com>
2020-12-28 01:04:02 -0800

    devices

--- src/Device.java
+++ src/Device.java
@@ -1,50 +1,29 @@
 import java.util.ArrayList;
 
 public class Device {
-    private float price;
-
-
+    private final float price;
     public String name;
-    private int minWeight;
-    private int minHeight;
-    private int minAge;
+    private final float minWeight;
+    private final float minHeight;
+    private final int minAge;
     private boolean isOpen;
-    private final ArrayList<eTicket> eTickets;
 
-    public Device(int minWeight, int minHeight, int minAge, boolean isOpen,String name,float price) {
+    public Device(float minWeight, float minHeight, int minAge, boolean isOpen,String name,float price) {
 
         this.minWeight = minWeight;
         this.minHeight = minHeight;
         this.minAge = minAge;
         this.isOpen = isOpen;
         this.price = price;
-        this.eTickets = new ArrayList<>();
-        this.name = name;
-        this.price = price;
-    }
-
-
-
-    public void setName(String name) {
         this.name = name;
     }
 
-    public void addETicket(eTicket ticket){
-        this.eTickets.add(ticket);
-    }
-
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
-
-
-    public int getMinWeight() {
+    public float getMinWeight() {
         return minWeight;
     }
 
 
-    public int getMinHeight() {
+    public float getMinHeight() {
         return minHeight;
     }
 
@@ -52,7 +31,6 @@ public class Device {
     public int getMinAge() {
         return minAge;
     }
-
 
     public boolean isOpen() {
         return isOpen;
